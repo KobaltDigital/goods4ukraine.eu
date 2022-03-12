147524c50dd8042d3c3f0f0514f1c611ba9bad12
Huub Duinmeijer <huub@kobaltdigital.nl>
2022-03-12 14:17:20 +0100

    Made more stuff dynamic

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,23 +16,35 @@
     <div class="w-full mb-10">
         <div class="max-w-2xl mx-auto">
             <div class="p-4">
-                <div>
-                    <label for="price" class="block text-sm font-medium text-gray-700">Location</label>
+                <form action="{{ route('home') }}" method="GET">
+                    <label for="location" class="block text-sm font-medium text-gray-700">
+                        {{ __('Location') }}
+                    </label>
                     <div class="relative mt-1 rounded-md shadow-sm">
-                        <input type="text" name="location" id="location"
+                        <input
+                            id="location"
+                            type="text"
+                            name="location"
                             class="block w-full pr-12 border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-3xl"
-                            placeholder="Alkmaar">
+                            placeholder="your location.."
+                        >
                         <div class="absolute inset-y-0 right-0 flex items-center">
-                            <label for="currency" class="sr-only">Distance</label>
-                            <select id="currency" name="currency"
+                            <label for="distance" class="sr-only">
+                                {{ __('Distance') }}
+                            </label>
+                            <select id="distance" name="distance"
                                 class="h-full py-0 pl-2 text-gray-500 bg-transparent border-transparent rounded-md focus:ring-indigo-500 focus:border-indigo-500 pr-7 sm:text-sm">
-                                <option>1km</option>
-                                <option>10km</option>
-                                <option>20km</option>
+                                <option value="5000">5{{ __('km') }}</option>
+                                <option value="15000">15{{ __('km') }}</option>
+                                <option value="25000">25{{ __('km') }}</option>
+                                <option value="50000">50{{ __('km') }}</option>
+                                <option value="100000">100{{ __('km') }}</option>
+                                <option value="500000">500{{ __('km') }}</option>
+                                <option value="null">{{ __('Any distance') }}</option>
                             </select>
                         </div>
                     </div>
-                </div>
+                </form>
 
             </div>
         </div>
@@ -48,18 +60,26 @@
                         class="block w-full pr-12 border-gray-300 rounded-md focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                         placeholder="Pindakaas">
                     <div class="absolute inset-y-0 right-0 flex items-center">
-                        <label for="currency" class="sr-only">Type</label>
-                        <select id="currency" name="currency"
+                        <label for="type" class="sr-only">
+                            {{ __('Type') }}
+                        </label>
+                        <select id="type" name="type"
                             class="h-full py-0 pl-2 text-gray-500 bg-transparent border-transparent rounded-md focus:ring-indigo-500 focus:border-indigo-500 pr-7 sm:text-sm">
-                            <option>alles</option>
-                            <option>aangeboden</option>
-                            <option>gevraagd</option>
+                            <option value="everything">{{ __('Everything') }}</option>
+                            <option value="offered">{{ __('Offered') }}</option>
+                            <option value="wanted">{{ __('Wanted') }}</option>
                         </select>
                     </div>
                 </div>
 
+                <div>
+                    <button type="submit" class="inline-flex items-center px-3 py-2 text-sm font-medium leading-4 text-white border border-transparent rounded-md shadow-sm bg-blue hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                        {{ __('Search') }}
+                    </button>
+                </div>
+
                 <div class="text-gray-400">
-                    <strong class="text-gray-500">{{ $ads->count() }}</strong> resultaten
+                    <strong class="text-gray-500">{{ $ads->count() }}</strong> {{ __('results') }}
                 </div>
             </div>
 
@@ -68,9 +88,4 @@
             @endforeach
         </div>
     </section>
-
-    <script>
-        console.log(google.places)
-    </script>
-
 </x-layout>
