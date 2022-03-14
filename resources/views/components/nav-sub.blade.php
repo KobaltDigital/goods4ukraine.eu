33169c10bb5f20d8c48156a7b3e2fa422398e106
Reinier Sierag <reinier@kobaltdigital.nl>
2022-03-14 23:44:27 +0100

    translated

--- resources/views/components/nav-sub.blade.php
+++ resources/views/components/nav-sub.blade.php
@@ -1,9 +1,9 @@
 <ul class="flex text-sm text-blue">
-    <li><a class="hover:bg-white hover:underline px-4 border py-2 rounded-full {{ request()->routeIs('about') ? 'border-accent bg-white' : 'border-white' }}" href="{{ route('about') }}" >Over Goods4ukraine</a></li>
-    <li><a class="hover:bg-white hover:underline px-4 border py-2 rounded-full {{ request()->routeIs('privacy') ? 'border-accent bg-white' : 'border-white' }}" href="{{ route('privacy'); }}">Privacyverklaring</a></li>
-    <li><a class="hover:bg-white hover:underline px-4 border py-2 rounded-full {{ request()->routeIs('cookies') ? 'border-accent bg-white' : 'border-white' }}" href="{{ route('cookies') }}">Cookiebeleid</a></li>
-    <li><a class="hover:bg-white hover:underline px-4 border py-2 rounded-full {{ request()->routeIs('rules') ? 'border-accent bg-white' : 'border-white' }}" href="{{ route('rules') }}">Verboden objecten</a></li>
-    <li><a class="hover:bg-white hover:underline px-4 border py-2 rounded-full {{ request()->routeIs('terms-of-use') ? 'border-accent bg-white' : 'border-white' }}" href="{{ route('terms-of-use') }}">Gebruikersvoorwaarden</a></li>
+    <li><a class="hover:bg-white hover:underline px-4 border py-2 rounded-full {{ request()->routeIs('about') ? 'border-accent bg-white' : 'border-white' }}" href="{{ route('about') }}" >{{ __("About") }}</a></li>
+    <li><a class="hover:bg-white hover:underline px-4 border py-2 rounded-full {{ request()->routeIs('privacy') ? 'border-accent bg-white' : 'border-white' }}" href="{{ route('privacy'); }}">{{ __("Privacy") }}</a></li>
+    <li><a class="hover:bg-white hover:underline px-4 border py-2 rounded-full {{ request()->routeIs('cookies') ? 'border-accent bg-white' : 'border-white' }}" href="{{ route('cookies') }}">{{ __("Cookies") }}</a></li>
+    <li><a class="hover:bg-white hover:underline px-4 border py-2 rounded-full {{ request()->routeIs('rules') ? 'border-accent bg-white' : 'border-white' }}" href="{{ route('rules') }}">{{ __("Rules") }}</a></li>
+    <li><a class="hover:bg-white hover:underline px-4 border py-2 rounded-full {{ request()->routeIs('terms-of-use') ? 'border-accent bg-white' : 'border-white' }}" href="{{ route('terms-of-use') }}">{{ __("Terms of use") }}</a></li>
 </ul>
 
 
