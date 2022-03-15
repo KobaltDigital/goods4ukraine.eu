383f59e9528ade7001da211303478d1756fd6d6e
Reinier Sierag <reinier@kobaltdigital.nl>
2022-03-16 00:11:25 +0100

    Better card and offered/wanted lables

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,4 +1,4 @@
-<div onclick="location.href = '{{ route('ad.show', $ad) }}';" class="cursur-pointer overflow-hidden bg-white shadow sm:rounded-lg hover:bg-light hover:shadow-2xl">
+<div onclick="location.href = '{{ route('ad.show', $ad) }}';" class="cursur-pointer overflow-hidden bg-white shadow sm:rounded-lg hover:shadow-2xl">
     <div class="px-4 py-5 border-t border-gray-200 sm:px-6">
         <dl class="grid grid-cols-1 gap-x-4 gap-y-8 sm:grid-cols-4">
             <div class="sm:col-span-1">
@@ -9,18 +9,14 @@
                     <div class="flex items-center justify-center space-x-4">
                         <div class="space-y-1">
                             <h3 class="font-serif text-lg font-medium leading-6 text-black">
+                                {{ __($ad->type) }}: 
+
                                 {!! $ad->title !!} <small class="font-sans text-gray-400">(4km)</small>
                             </h3>
                             <p class="text-[10px] text-blue underline">Tag 1, Tag 2</p>
                         </div>
                     </div>
-                    <div class="flex justify-end">
-                        <span class="inline-flex items-center px-2.5 py-0.5 rounded-full text-xs font-medium bg-gray-100 text-gray-800">
-                            {{ __($ad->type) }}
-                        </span>
-                    </div>
                     <x-button :href="route('ad.show', $ad)">{{ __('View') }}</x-button>
-
                 </div>
             
                 <dd class="mt-1 text-sm  text-black">
