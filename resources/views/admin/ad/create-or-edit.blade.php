540def11bafbd7ca3bc270660f717350be739e13
Reinier Sierag <reinier@kobaltdigital.nl>
2022-03-23 22:41:41 +0100

    Image example

--- resources/views/admin/ad/create-or-edit.blade.php
+++ resources/views/admin/ad/create-or-edit.blade.php
@@ -53,13 +53,15 @@
                             <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                                 <label for="cover-photo" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2">{{ __('Photo') }}</label>
                                 <div class="mt-1 sm:mt-0 sm:col-span-2">
+                                    <div id="image-grid" class="bg-gray-400">
+                                    </div>
                                     <div class="flex justify-center max-w-lg px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
                                         <div class="space-y-1 text-center">
                                             <svg class="w-12 h-12 mx-auto text-gray-400" stroke="currentColor" fill="none" viewBox="0 0 48 48" aria-hidden="true">
                                                 <path d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
                                             </svg>
                                             <div class="flex text-sm text-gray-600">
-                                                <label for="file-upload" class="relative font-medium text-indigo-600 bg-white rounded-md cursor-pointer hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                                                <label for="file-upload" class="relative font-medium text-blue bg-white rounded-md cursor-pointer hover:text-blue focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-blue-500">
                                                     <span>{{ __('Choose File') }}</span>
                                                     <input id="file-upload" name="file-upload" type="file" accept="image/*" class="sr-only">
                                                 </label>
@@ -72,6 +74,22 @@
                             </div>
                         </div>
                     </div>
+                    <script>
+                    const fileUploader = document.getElementById('file-upload');
+                    const reader = new FileReader();
+                    const imageGrid = document.getElementById('image-grid');
+
+                    fileUploader.addEventListener('change', (event) => {
+                        const files = event.target.files;
+                        const file = files[0];
+                        
+                        const img = document.createElement('img');
+                        imageGrid.appendChild(img);
+                        img.src = URL.createObjectURL(file);
+                        img.alt = file.name;
+                    });
+
+                    </script>
 
                     <div class="mt-6 md:mt-0">
                         <h3>{{ __('Contact information') }}</h3>
